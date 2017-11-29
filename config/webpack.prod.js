6f8dab68eb84f085664bcab2f7f49637766b3189
CORPDOM1\MLostaunau <mlostaunau@newgistics.com>
2017-11-29 15:23:23 -0600

    Testing different public path

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = webpackMerge(commonConfig, {
 
   output: {
     path: helpers.root('dist'),
-    publicPath: 'http://0.0.0.0:8080',
+    publicPath: 'http://localhost:8080',
     filename: '[name].[hash].js',
     chunkFilename: '[id].[hash].chunk.js'
   },
